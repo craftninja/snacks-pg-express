acdf01c78179615a44b54fbf9b0d700ef170f512
Emily Platzer <emilyplatzer@gmail.com>
2016-08-28 16:04:27 -0600

    Added production config

--- knexfile.js
+++ knexfile.js
@@ -2,5 +2,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/snacks_development'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 };
